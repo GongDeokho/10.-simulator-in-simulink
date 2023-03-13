fc19316d987352bee08081f038b8587bafd125c8
GongDeokho <rhdejrgh15@knu.ac.kr>
2023-03-14 05:41:10 +0900

    완성 version_1

--- test1.m
+++ test1.m
@@ -1,37 +1,26 @@
 clc
 clear all
-count = 1
+evse = zeros(12,4);
+evse(1,:) = 1;
+evse(2,:) = [0 5 0 2];
+evse(3,:) = [79.2 80.3 65.8 72.4];
+evse(4,:) = [0.45 0.46 0.64 0.56];
+evse(5,:) = [0.84 0.54 0.69 0.74];
+evse(6,:) = [0.05 0.05 0.05 0.05];
+evse(7,:) = [0.95 0.95 0.95 0.95];
+evse(8,:) = [9 5 10 9];
+evse(9,:) = [-5 -5 -5 -5];
+evse(10,:) = [11 11 11 11];
+evse(11,:) = [83 83 83 83];
+evse(12,:) = [83 83 83 83];
+% 운용 가능 플러그 개수 확인하기
+idx = find(evse(1,:)==0);
+empty_num = size(find(evse(1,:) == 0),2);
 
-% header = {"time", "capacity", "init_soc", "target", "dur", "min", "max", "Pmin", "Pmax", "chg_eff", "dch_eff", "test1", "test2"};
-evse = zeros(11,3);
-evse(:,2) = 1;
-N = 10;
-% for k=1:N
-%     my_field = strcat('v',num2str(k));
-%     variable.(my_field) = k*2;
-% end
-% 
-% for i = 1:size(evse,2)
-%     if (evse(1,i) == 1) && (sum(evse(2:size(evse,1),i)) ~= 0)
-%         for j = 1:size(evse,1)
-%             variable.(sprintf('v%d',i+1)) = transpose(evse(:,i));
-%             variable.v3 = array2table(variable.v3,"VariableNames",header{1});
-% 
-%         end
-%     end
-% end
-if count == 1
-    for i=1:size(evse,2)
-        data_evse = sprintf('v%d',i);
-        evse_1.(data_evse) = nan;
-    end
+% 운용 가능 플러그 개수만큼 분포 발생하기
+if sum(empty_num) ~= 0
+   for i = 1:empty_num
+       evse(1,idx(i)) = randi([0 1]);
+   end
 end
-
-for i = 1:size(evse,2)
-    if (evse(1,i) == 1) && (sum(evse(2:size(evse,1),i)) ~= 0)
-        for t = 1:size(evse,2)
-            evse_1.(sprintf('v%d',i+1)) = transpose(evse(:,i));
-            evse_1.(sprintf('v%d',i+1)) = array2table(evse_1.(sprintf('v%d',i+1)),"VariableNames",["time", "capacity", "init_soc", "target", "min", "max", "dur", "Pmin", "Pmax", "chg_eff", "dch_eff"]);
-        end
-    end
-end
+y = evse;
